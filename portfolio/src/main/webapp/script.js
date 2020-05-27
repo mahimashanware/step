aebc6c93d8db96cab211aa48747bffd1588a44b2
Mahima Shanware <mahima.shanware@gmail.com>
2020-05-27 16:32:43 -0700

    Create json string in server

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -34,3 +34,16 @@ function getRandomDataUsingArrowFunctions() {
   });
   console.log('Done.');
 }
+
+function getData() {
+  fetch('/data').then(response => response.json()).then((data) => {
+    // stats is an object, not a string, so we have to
+    // reference its fields to create HTML content
+
+    const dataListElement = document.getElementById('data-container');
+    dataListElement.innerHTML = '';
+    dataListElement.appendChild(data);
+  });
+}
+
+
