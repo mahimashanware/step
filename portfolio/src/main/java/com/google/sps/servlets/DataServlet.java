d47065e5b84fb889b60e3f9210ff9e4f5c30ee3d
Mahima Shanware <mahima.shanware@gmail.com>
2020-05-27 16:32:43 -0700

    Create json string in server

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -16,38 +16,31 @@ package com.google.sps.servlets;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import java.util.*;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public final class DataServlet extends HttpServlet {
 
-  private List<String> datas;
-
-  @Override
-  public void init() {
-    datas = new ArrayList<>();
-    datas.add(
-        "I love coffee and my favorite chain is Philz!");
-    datas.add("My birthday is on July 19.");
-    datas.add("My favorite show is Game of Thrones.");
-    datas.add("I went to the Grace Hopper Conference last year!");
-    datas.add("My favorite class at CMU was 15-112 Fundamentals of Programming!");
-    datas.add("My favorite book is Catcher in the Rye.");
-    datas.add("One thing I want to learn more about is machine learning.");
-    datas.add("My ideal job is working on accessibility software for self-driving cars.");
-    datas.add("My favorite place at CMU to study is the Tepper Quad.");
-  }
+    private ArrayList<String> object = new ArrayList<String>(); 
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String data = datas.get((int) (Math.random() * datas.size()));
+    object.add("one");
+    object.add("two");
+    String json = convertToJson(object);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
 
-    response.setContentType("text/html;");
-    response.getWriter().println(data);
+  private String convertToJson(ArrayList object) {
+    Gson gson = new Gson();
+    String json = gson.toJson(object);
+    return json;
   }
 }
